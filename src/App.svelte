db5c14e444103586448ff9f7a8e4ed3fd8d08a19
dmiloloza <domagoj.miloloza@gmil.com>
2020-09-09 16:16:46 +0200

    #9 - CSS & Conditional Styles

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,7 @@
             beltColor: 'brown',
             age: 35,
             id: 3
-        },{
+        }, {
             name: 'Bowser',
             beltColor: 'black',
             age: 37,
@@ -41,54 +41,52 @@
 </script>
 
 <Modal/>
-<main>
-    <p>{fullName} - {beltColor} belt</p>
-    <input bind:value={firstName} type="text">
-    <input bind:value={lastName} type="text">
-    <input bind:value={beltColor} type="text">
-    <hr>
-    {#if people.length === 1 }
-        <p>Solo ninja</p>
-    {:else if people.length <=3}
-        <p>Small team</p>
-    {:else}
-        <p>Ninja squad</p>
-    {/if}
-    <hr>
-    <div>
-        {#each people as person (person.id)}
-            <div>
-                <h4>{person.name}</h4>
-                {#if person.beltColor === 'black'}
-                    <p><strong>MASTER NINJA</strong></p>
-                {/if}
-                <p>{person.age} years old, {person.beltColor} belt</p>
-                <button on:click={() => handleClick(person.id)}>delete</button>
-            </div>
+    <main>
+        <p>{fullName} - {beltColor} belt</p>
+        <input bind:value={firstName} type="text">
+        <input bind:value={lastName} type="text">
+        <input bind:value={beltColor} type="text">
+        <hr>
+        {#if people.length === 1 }
+            <p>Solo ninja</p>
+        {:else if people.length > 1}
+            <p>Ninja squad</p>
         {:else}
-            <p>There are no people to show!</p>
-        {/each}
-    </div>
-</main>
+        {/if}
+        <div>
+            {#each people as person (person.id)}
+                <div>
+                    <h4>{person.name}</h4>
+                    {#if person.beltColor === 'black'}
+                        <p><strong>MASTER NINJA</strong></p>
+                    {/if}
+                    <p>{person.age} years old, {person.beltColor} belt</p>
+                    <button on:click={() => handleClick(person.id)}>delete</button>
+                </div>
+            {:else}
+                <p>There are no Ninja to show!</p>
+            {/each}
+        </div>
+    </main>
 
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
+    <style>
         main {
-            max-width: none;
+            text-align: center;
+            padding: 1em;
+            max-width: 240px;
+            margin: 0 auto;
         }
-    }
-</style>
+
+        h1 {
+            color: #ff3e00;
+            text-transform: uppercase;
+            font-size: 4em;
+            font-weight: 100;
+        }
+
+        @media (min-width: 640px) {
+            main {
+                max-width: none;
+            }
+        }
+    </style>
